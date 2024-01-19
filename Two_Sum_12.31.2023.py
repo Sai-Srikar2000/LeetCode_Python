4f9396761227251a705f93b88537e5205b810ff0
saisr <git@github.com:Sai-Srikar2000/LeetCode_Python.git>
2024-01-19 00:26:48 -0600

    first_trial

---
+++ Two_Sum_12.31.2023.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import time
+
+# given_array = [1,4,10,-3]
+# for i in given_array:
+#     a=given_array[0]
+#     b=given_array[3]
+#     print(a,b)
+nums = [1,2,3]
+for i in nums:
+    a = i + 5
+    print(a)
+    time.sleep(2)
+
